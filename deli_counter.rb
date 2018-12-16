f23e9fbee91505509000d961a0586755398c5844
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-16 23:36:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(katz_deli)
+  line_position = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index { |i, index| line_position << "#{index+1}. #{i}" }
+    puts "The line is currently: #{line_position.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli,string)
+  katz_deli.push(string)
+  katz_deli.each_with_index { |i, index| puts "Welcome, #{i}. You are number #{index+1} in line." }
+end
+
+
+
+
+
+
+
+
+
+
+
