a65128a4b920164371fa53981f7f3a6a92632e88
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-17 00:21:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,11 @@ def take_a_number(katz_deli,string)
   puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  "There is nobody waiting to be served!"
+def now_serving(katz_deli)
+  if katz_deli = []
+    puts "There is nobody waiting to be served!"
+  elsif
+    
 
 
 
